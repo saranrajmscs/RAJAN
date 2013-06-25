0b43ee1d34ae8c81b8613e18d19f8c0cef459c8f
saranrajmscs <saranraj@gmail.com>
2013-06-24 22:03:08 -0400

    Changes Create Invitation Module

--- Social Event Planner/src/com/sep/Social_Event_PlannerServlet.java
+++ Social Event Planner/src/com/sep/Social_Event_PlannerServlet.java
@@ -1,6 +1,7 @@
 package com.sep;
 
 import java.io.IOException;
+import java.sql.*;
 import javax.servlet.http.*;
 
 @SuppressWarnings("serial")
@@ -11,5 +12,19 @@ public class Social_Event_PlannerServlet extends HttpServlet {
 		resp.getWriter().println("<h1><font face=\"Verdana\"> Social Event Planner </font></h1><br/>");
 		// Test Message for DVCS
 		resp.getWriter().println("<font face=\"Verdana\"> This is the first servlet. Our logic will be built on this</font><br/> ");
+		try {
+			SEP_DB_Manager sepDB = new SEP_DB_Manager();
+		Connection c = SEP_DB_Manager.getConnection();
+		System.out.println("CONNECTION : " + c);
+		String statement ="select * from USER_MASTER";
+	    PreparedStatement stmt = c.prepareStatement(statement);
+	      ResultSet rs = stmt.executeQuery();
+	      resp.getWriter().println(rs.findColumn("ROW_ID"));
+	      resp.getWriter().println(req.getParameter("inputEmail"));
+	      
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
